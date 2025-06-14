ddd7a5a12a141d36801d623f4cafe12b6646f36d
Ritvik Anand <128292481+RitvikAnand583@users.noreply.github.com>
2025-06-14 10:54:23 +0530

    design changed

--- client/sidebarProvider.js
+++ client/sidebarProvider.js
@@ -51,12 +51,14 @@ class SidebarProvider {
                 <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link href="${styleUri}" rel="stylesheet">
-                <title>LLM Tools</title>
+                <title>AUTO-DOC</title>
             </head>
             <body>
-                <button class="action-button" id="project-btn">Project to File</button>
-                <button class="action-button" id="window-btn">Active Window to File</button>
-                <button class="action-button" id="readme-btn">Generate README</button>
+                <button class="action-button" id="project-btn">Project Doc</button>
+                <button class="action-button" id="window-btn">Active Tab Doc</button>
+                <button class="action-button" id="activeTab-flow-btn">Active Tab Flow</button>
+                <button class="action-button" id="create-flow-btn">Get Flow Chart</button>
+                <button class="action-button" id="readme-btn">Get README.md</button>
 
                 <script nonce="${nonce}" src="${scriptUri}"></script>
             </body>
