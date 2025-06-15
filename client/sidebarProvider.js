7de46bfec313d4b9ba74840824e6015019dff576
Ritvik Anand <128292481+RitvikAnand583@users.noreply.github.com>
2025-06-15 10:56:29 +0530

    design changed2

--- client/sidebarProvider.js
+++ client/sidebarProvider.js
@@ -40,32 +40,69 @@ class SidebarProvider {
         const scriptUri = webview.asWebviewUri(
             vscode.Uri.joinPath(this._extensionUri, "media", "main.js"),
         );
-        const styleUri = webview.asWebviewUri(
-            vscode.Uri.joinPath(this._extensionUri, "media", "main.css"),
+
+        const styleMainUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
         );
+        
+        const codiconsUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'node_modules', '@vscode/codicons', 'dist', 'codicon.css'));
+
+        const logoUri = webview.asWebviewUri(
+            vscode.Uri.joinPath(this._extensionUri, "media", "logo.svg")
+        );
+    
 
         // Use a nonce to only allow specific scripts to be run
         const nonce = getNonce();
 
         return `<!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link href="${styleUri}" rel="stylesheet">
-                <title>AUTO-DOC</title>
-            </head>
-            <body>
-                <button class="action-button" id="project-btn">Project Doc</button>
-                <button class="action-button" id="window-btn">Active Tab Doc</button>
-                <button class="action-button" id="activeTab-flow-btn">Active Tab FlowChart</button>
-                <button class="action-button" id="create-flow-btn">Get FlowChart</button>
-                <button class="action-button" id="readme-btn">Get README.md</button>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; font-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            
+            <link href="${codiconsUri}" rel="stylesheet" />
+            <link href="${styleMainUri}" rel="stylesheet">
+            
+            <title>Auto-Doc</title>
+        </head>
+        <body>
+            <div class="header">
+                <h3>Auto-Doc</h3>
+            </div>
 
-                <script nonce="${nonce}" src="${scriptUri}"></script>
-            </body>
-            </html>`;
+            <div class="action-group">
+                <h4><i class="codicon codicon-book"></i> Documentation</h4>
+                <button class="action-button" id="project-btn">
+                    <i class="codicon codicon-folder-library"></i>
+                    <span>Project Docs</span>
+                </button>
+                <button class="action-button" id="window-btn">
+                    <i class="codicon codicon-go-to-file"></i>
+                    <span>Active File Docs</span>
+                </button>
+                <button class="action-button" id="readme-btn">
+                    <i class="codicon codicon-markdown"></i>
+                    <span>Generate README.md</span>
+                </button>
+            </div>
+
+            <div class="action-group">
+                <h4><i class="codicon codicon-symbol-class"></i> Visualizations</h4>
+                <button class="action-button" id="activeTab-flow-btn">
+                    <i class="codicon codicon-symbol-method"></i>
+                    <span>Active File Flowchart</span>
+                </button>
+                <button class="action-button" id="create-flow-btn">
+                     <i class="codicon codicon-project"></i>
+                    <span>Project Flowchart</span>
+                </button>
+            </div>
+
+            <script nonce="${nonce}" src="${scriptUri}"></script>
+        </body>
+        </html>`;
     }
 }
 
